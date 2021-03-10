88e39d9fe777ac5412d3636efe5df5c6c592e116
maki-kajiya <sentorei.5.8.11.seven@gmail.com>
2021-03-10 20:27:00 +0900

    RadioButtonとTextFieldのFormKeyを設定

--- lib/ui/pages/confidence_page.dart
+++ lib/ui/pages/confidence_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:new_scrum_poker/UI/common_components/space_box.dart';
+import 'package:new_scrum_poker/ui/common_components/radiobutton.dart';
 import 'package:new_scrum_poker/ui/controllers/confidence_controller.dart';
 
 class ConfidenceDialog extends StatefulWidget {
@@ -12,8 +13,10 @@ class ConfidenceDialog extends StatefulWidget {
 
 class _ConfidenceDialogState extends State<ConfidenceDialog> {
   static final _contentsBackgroundColor = Colors.teal[100];
-  static final _formKey = GlobalKey<FormState>();
-  final ProductBacklogItem _form = ProductBacklogItem();
+  final _radioFormKey = GlobalKey<FormState>();
+  final _textFormKey = GlobalKey<FormState>();
+
+  ProductBacklogItem _form = ProductBacklogItem();
   var _textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -35,32 +38,29 @@ class _ConfidenceDialogState extends State<ConfidenceDialog> {
                       right: 0.1,
                       left: 0.1,
                     ),
-                    child: Form(
-                      key: _formKey,
-                      child: Column(
-                        children: <Widget>[
-                          SpaceBox.height(),
-                          Text(
-                            widget.storyPoint,
-                            style: TextStyle(
-                              color: _contentsBackgroundColor,
-                              fontSize: 45.0,
-                            ),
+                    child: Column(
+                      children: <Widget>[
+                        SpaceBox.height(),
+                        Text(
+                          widget.storyPoint,
+                          style: TextStyle(
+                            color: _contentsBackgroundColor,
+                            fontSize: 45.0,
                           ),
-                          SpaceBox.height(),
-                          Text(
-                            '自信はありますか？',
-                            style: TextStyle(color: Colors.grey, fontSize: 20),
-                          ),
-                          SpaceBox.height(),
-                          _buildRadioButtonList(),
-                          SpaceBox.height(10),
-                          _buildNameField(false),
-                          SpaceBox.height(40),
-                          _buildButton(context),
-                          SpaceBox.height(),
-                        ],
-                      ),
+                        ),
+                        SpaceBox.height(),
+                        Text(
+                          '自信はありますか？',
+                          style: TextStyle(color: Colors.grey, fontSize: 20),
+                        ),
+                        SpaceBox.height(),
+                        _buildRadioButtonList(),
+                        SpaceBox.height(10),
+                        _buildNameField(false),
+                        SpaceBox.height(40),
+                        _buildButton(context),
+                        SpaceBox.height(),
+                      ],
                     ),
                   ),
                 ),
@@ -72,32 +72,38 @@ class _ConfidenceDialogState extends State<ConfidenceDialog> {
     );
   }
 
-  List<String> confidentList = ['自信あり', '普通', '自信なし'];
-  List<int> confidentValueList = [1, 2, 3];
+  List<String> _confidentList = ['自信あり', '普通', '自信なし'];
+  List<int> _confidentValueList = [1, 2, 3];
   int _currentConfident = 2;
 
   Widget _buildRadioButtonList() {
-    return ListView.builder(
-      shrinkWrap: true,
-      itemCount: confidentList.length,
-      itemBuilder: (BuildContext context, int index) {
-        return RadioListTile(
-          value: confidentValueList[index],
-          activeColor: _contentsBackgroundColor,
-          groupValue: _currentConfident,
-          title: Text(confidentList[index]),
-          onChanged: (val) {
-            setState(
-              () {
-                // TODO : print消す
-                _currentConfident = val;
-                _form.confidentDegree = val;
-                print(_form.confidentDegree);
-              },
-            );
-          },
-        );
-      },
+    return Form(
+      key: _radioFormKey,
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: _confidentList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return RadioListTileField(
+            confidentList: _confidentList,
+            confidentValueList: _confidentValueList,
+            currentConfident: _currentConfident,
+            contentsBackgroundColor: _contentsBackgroundColor,
+            index: index,
+            onChanged: (val) {
+              setState(
+                () {
+                  _currentConfident = val;
+                  print(_currentConfident);
+                },
+              );
+            },
+            onSaved: (val) {
+              _form.confidentDegree = _currentConfident;
+              print(_form.confidentDegree);
+            },
+          );
+        },
+      ),
     );
   }
 
@@ -114,10 +120,13 @@ class _ConfidenceDialogState extends State<ConfidenceDialog> {
           ),
           Expanded(
             flex: 7,
-            child: TextFormField(
-              controller: _textEditingController,
-              onSaved: (value) => _form.name = _textEditingController.text,
-              textInputAction: TextInputAction.done,
+            child: Form(
+              key: _textFormKey,
+              child: TextFormField(
+                controller: _textEditingController,
+                onSaved: (value) => _form.name = _textEditingController.text,
+                textInputAction: TextInputAction.done,
+              ),
             ),
           ),
         ],
@@ -137,12 +146,8 @@ class _ConfidenceDialogState extends State<ConfidenceDialog> {
         shape: const StadiumBorder(),
         onPressed: () {
           _form.storyPoint = widget.storyPoint;
-          _formKey.currentState.save();
-          // TODO : print消す
-          print(_form.storyPoint);
-          print(_form.name);
-          print(_form.confidentDegree);
-
+          _radioFormKey.currentState.save();
+          _textFormKey.currentState.save();
           ConfidenceController().send(context, _form);
         },
       ),
