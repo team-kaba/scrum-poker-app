43050d3fccd2313d0b30bed2b63558be36c3f019
maki-kajiya <sentorei.5.8.11.seven@gmail.com>
2021-03-11 15:52:26 +0900

    ResultPageにローディング対応。動作確認してない。

--- lib/ui/pages/result_page.dart
+++ lib/ui/pages/result_page.dart
@@ -3,7 +3,9 @@ import 'dart:ui';
 import 'package:bubble/bubble.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:new_scrum_poker/servicies/get_backlog_item_service.dart';
 import 'package:new_scrum_poker/ui/common_components/space_box.dart';
+import 'package:provider/provider.dart';
 
 class ResultPage extends StatefulWidget {
   ResultPage({Key key}) : super(key: key);
@@ -14,12 +16,33 @@ class ResultPage extends StatefulWidget {
 
 class _ResultPageState extends State<ResultPage> {
   final _contentsBackgroundColor = Colors.teal[100];
+  bool _isLoading = false;
+
+  @override
+  void initState() {
+    Future.delayed(Duration.zero).then((_) async {
+      setState(() {
+        _isLoading = true;
+      });
+      // TODO: controllerクラスでserviceクラスを呼び出す。
+      // ローディングも？State管理だめ
+      dynamic response = await GetBacklogItemServise().getBacklogItems();
+      setState(() {
+        _isLoading = false;
+      });
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(context),
-      body: _buildBody(context),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : _buildBody(context),
     );
   }
 
@@ -34,7 +57,8 @@ class _ResultPageState extends State<ResultPage> {
         ),
         onPressed: () => Navigator.of(context).pop(),
       ),
-      title: Text('Result', style: Theme.of(context).textTheme.headline3),
+      title:
+          Text('Result', style: TextStyle(fontSize: 40, color: Colors.white)),
       centerTitle: true,
       elevation: 0.0,
       brightness: Brightness.light,
@@ -52,6 +76,7 @@ class _ResultPageState extends State<ResultPage> {
     );
   }
 
+  // TODO:APIのレスポンスをListに入れる
   List<int> storyPointList = [3, 2, 3, 1];
   List<String> confidentList = ['自信あり', '普通', '自信なし', '普通'];
   List<String> nameList = ['maki', '牧', '巻き', 'カジマキ'];
@@ -71,26 +96,33 @@ class _ResultPageState extends State<ResultPage> {
                 Column(
                   children: <Widget>[
                     SpaceBox.height(20),
-                    Text(nameList[index]),
+                    Text(nameList[index],
+                        style: TextStyle(fontSize: 18, color: Colors.white)),
                     Icon(
-                      Icons.face,
-                      size: 40,
+                      // TODO:confident_degreeの値に応じて顔文字のアイコンを変える
+                      // icons:sentiment_very_satisfied = smile, sentiment_neutral = neutral, sentiment_very_dissatisfied = bad
+                      Icons.sentiment_very_dissatisfied,
+                      size: 50,
+                      color: Colors.white,
                     )
                   ],
                 ),
                 Bubble(
-                  margin: BubbleEdges.only(top: 30, left: 10),
-                  padding: BubbleEdges.all(20),
+                  margin: BubbleEdges.only(top: 30, left: 20),
+                  padding: BubbleEdges.symmetric(horizontal: 50, vertical: 20),
                   alignment: Alignment.topLeft,
                   nip: BubbleNip.leftBottom,
                   child: Column(
                     children: <Widget>[
-                      Text('Story Point : ' + storyPointList[index].toString()),
-                      Text(confidentList[index]),
+                      Text(
+                        storyPointList[index].toString(),
+                        style: TextStyle(fontSize: 40, color: Colors.grey[500]),
+                      ),
+                      // Text(confidentList[index]),
                     ],
                   ),
-                  shadowColor: Colors.blueGrey,
-                  elevation: 10,
+                  // shadowColor: Colors.blueGrey,
+                  // elevation: 0,
                 ),
               ],
             );
