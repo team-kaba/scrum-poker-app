017642388aa332eb70d848d4dff2592fbf97b95e
maki-kajiya <sentorei.5.8.11.seven@gmail.com>
2021-04-02 17:46:37 +0900

    クラス名変更

--- lib/ui/controllers/confidence_controller.dart
+++ lib/ui/controllers/confidence_controller.dart
@@ -3,20 +3,22 @@ import 'package:new_scrum_poker/servicies/product_backlog_item_service.dart';
 
 class ConfidenceController with ChangeNotifier {
   ConfidenceController();
-  void send(BuildContext context, ProductBacklogItem productBacklogItem) {
+  void send(BuildContext context, ProductBacklogItemForm productBacklogItem) {
     ProductBacklogItemService().send(ProductBacklogItemRequest(
       story_point: productBacklogItem.storyPoint,
       name: productBacklogItem.name,
       confident_degree: productBacklogItem.confidentDegree,
     ));
-    // ダイアログを閉じる
+    // ConfidenceDialogを閉じる
     Navigator.pop(context);
+
     Navigator.pushNamed(context, '/result');
   }
 }
 
-class ProductBacklogItem {
-  ProductBacklogItem({this.storyPoint, this.name, this.confidentDegree});
+// FIXME:このクラスを書く場所はここじゃない気がする
+class ProductBacklogItemForm {
+  ProductBacklogItemForm({this.storyPoint, this.name, this.confidentDegree});
   String storyPoint;
   String name;
   int confidentDegree;
