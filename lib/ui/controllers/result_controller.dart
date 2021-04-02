fe987bbd2b407d46c95fde6f8f76c48fc72b249e
maki-kajiya <sentorei.5.8.11.seven@gmail.com>
2021-04-02 17:50:02 +0900

    ResultController作成

---
+++ lib/ui/controllers/result_controller.dart
@@ -0,0 +1,21 @@
+import 'package:new_scrum_poker/servicies/get_backlog_item_service.dart';
+import 'package:new_scrum_poker/ui/controllers/result_state.dart';
+import 'package:state_notifier/state_notifier.dart';
+
+class ResultController extends StateNotifier<ResultState> {
+  ResultController() : super(ResultState());
+
+  Future<void> getBacklogItems() async {
+    final response = await GetBacklogItemServise().fetch();
+    final getBacklogItems = response.backlogItems;
+    // ここ理解できていない（.map()メソッドがわかっていない）
+    final viewModels = getBacklogItems
+        .map((n) => GetBacklogItemViewModel(
+              name: n.name,
+              storyPoint: n.story_point,
+              confidentDegree: n.confident_degree,
+            ))
+        .toList();
+    state = state.copyWith(backlogItemsViewModel: viewModels);
+  }
+}
